615b020eb0154423c05a3456dd0ed3409818b0cc
Nilseu Padilha <ragnarokkrr.blog@gmail.com>
2013-08-21 20:10:54 -0300

    T003 - Implementar Classe Pessoa e atributos

---
+++ src/edu/faqi/ipsoo/gc/Pessoa.java
@@ -0,0 +1,26 @@
+package edu.faqi.ipsoo.gc;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+
+	public Pessoa(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", idade=" + idade + "]";
+	}
+
+}
